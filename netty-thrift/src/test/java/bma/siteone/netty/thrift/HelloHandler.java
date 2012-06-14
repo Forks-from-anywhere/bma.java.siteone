bf87b11e1c08423bd6135810e46475bd046ffc48
guan zhong <catyguan@gmail.com>
2012-06-14 10:41:01 +0800

    TAIBaseProcessor

--- netty-thrift/src/test/java/bma/siteone/netty/thrift/HelloHandler.java
+++ netty-thrift/src/test/java/bma/siteone/netty/thrift/HelloHandler.java
@@ -3,7 +3,12 @@ package bma.siteone.netty.thrift;
 import org.apache.thrift.TApplicationException;
 import org.apache.thrift.TException;
 
-public class HelloHandler implements Hello.Iface {
+import bma.common.langutil.ai.stack.AISRun;
+import bma.common.langutil.ai.stack.AIStack;
+import bma.common.thrift.TAIBaseFace;
+
+public class HelloHandler extends TAIBaseFace implements Hello.Iface,
+		Hello4AI.Iface {
 
 	final org.slf4j.Logger log = org.slf4j.LoggerFactory
 			.getLogger(HelloHandler.class);
@@ -16,6 +21,19 @@ public class HelloHandler implements Hello.Iface {
 	}
 
 	@Override
+	public boolean say(AIStack<Boolean> stack, final String word) {
+		return executeAt(stack, new AISRun<Boolean>() {
+			@Override
+			public void execute() {
+				if (log.isDebugEnabled()) {
+					log.debug("ai-hello {}", word);
+				}
+				success(true);
+			}
+		});
+	}
+
+	@Override
 	public String name(String title) throws TException {
 		if (log.isDebugEnabled()) {
 			log.debug("name: {}", title);
@@ -24,11 +42,38 @@ public class HelloHandler implements Hello.Iface {
 	}
 
 	@Override
+	public boolean name(AIStack<String> stack, final String title) {
+		return executeAt(stack, new AISRun<String>() {
+			@Override
+			public void execute() {
+				if (log.isDebugEnabled()) {
+					log.debug("ai-name: {}", title);
+				}
+				String s = "hi," + title + " guan";
+				success(s);
+			}
+		});
+	}
+
+	@Override
 	public void error(String msg) throws TException {
 		if (log.isDebugEnabled()) {
 			log.debug("error: {}", msg);
 		}
 		throw new TApplicationException("error: " + msg);
+	}
+
+	@Override
+	public boolean error(AIStack<Boolean> stack, final String msg) {
+		return executeAt(stack, new AISRun<Boolean>() {
+			@Override
+			public void execute() {
+				if (log.isDebugEnabled()) {
+					log.debug("error: {}", msg);
+				}
+				failure(new TApplicationException("error: " + msg));
+			}
+		});
 	}
 
 }
