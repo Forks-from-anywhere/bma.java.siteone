da492ef19f499e6593a1a052ccca538c0795f603
guan zhong <catyguan@gmail.com>
2012-11-27 16:53:14 +0800

    move netty-thrift

--- netty-thrift/src/main/java/bma/siteone/netty/thrift/gate/impl/RuntimeRemoteAware.java
+++
@@ -1,8 +0,0 @@
-package bma.siteone.netty.thrift.gate.impl;
-
-import bma.siteone.netty.thrift.remote.RuntimeRemote;
-
-public interface RuntimeRemoteAware {
-
-	public void setRuntimeRemote(RuntimeRemote rr);
-}
--- netty-thrift/src/main/java/bma/siteone/netty/thrift/remote/impl/TRemoteInfoBuilder.java
+++
@@ -1,8 +0,0 @@
-package bma.siteone.netty.thrift.remote.impl;
-
-import bma.siteone.netty.thrift.remote.thrift.TRemoteInfo;
-
-public interface TRemoteInfoBuilder {
-
-	public void build(TRemoteInfo info);
-}
